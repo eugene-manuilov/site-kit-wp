653a9b4d6b79454c46d836f3edfabacc3ba8dfa1
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-11-04 10:00:45 +0200

    Update workflow file.

--- .github/actions/wordpress/down.js
+++ .github/actions/wordpress/down.js
@@ -1,6 +1,8 @@
 const core = require( '@actions/core' );
 
 try {
+	const container_id = core.getState( 'container_id' );
+	console.log( container_id );
 } catch ( e ) {
 	core.setFailed( e.message );
 }
--- .github/actions/wordpress/up.js
+++ .github/actions/wordpress/up.js
@@ -1,9 +1,16 @@
 const core = require( '@actions/core' );
 
 try {
-	const container_id = '12345';
+	const image = core.getInput( 'image' );
+	const imageTag = core.getInput( 'image-tag' );
+	const username = core.getInput( 'image-username' );
+	const password = core.getInput( 'image-password' );
 
 	console.log( __dirname );
+	console.log( image + ':' + imageTag );
+	console.log( username );
+
+	const container_id = '12345';
 
 	core.saveState( 'container_id', container_id );
 	core.setOutput( 'container_id', container_id );
