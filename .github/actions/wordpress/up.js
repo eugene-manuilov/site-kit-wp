934bf7cabd82109a68907bc7207b8beb1b064cd6
Eugene Manuilov <eugene.manuilov@gmail.com>
2020-11-04 12:38:22 +0200

    Update workflow file.

--- .github/actions/wordpress/up.js
+++ .github/actions/wordpress/up.js
@@ -5,18 +5,23 @@ try {
 	const username = core.getInput( 'image-username' );
 	const password = core.getInput( 'image-password' );
 
+	console.log( core.getInput( 'container' ) );
+
 	// const docker = new Docker();
 
 	// core.startGroup( 'Start WordPress container' );
+
 	// docker.createContainer( {
 	// 	name: 'wordpress',
 	// 	Image: core.getInput( 'image', { required: true } ) + ':' + core.getInput( 'image-tag', { required: true } ),
-	// 	ExposedPorts: {
-	// 		9002: {
-
-	// 		}
-	// 	}
+	// 	HostConfig: {
+	// 		NetworkMode: core.getInput( 'image-network', { required: true } ),
+	// 		PortBindings: {
+	// 			'9002/tcp': [ { HostPort: '80' } ],
+	// 		},
+	// 	},
 	// } );
+
 	// core.endGroup();
 
 	const container_id = '12345';
